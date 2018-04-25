35cc429c72bb1d628778b0b855cbefe1daddd974
Noeline <noeline.wyart@viacesi.fr>
2018-04-25 11:19:12 +0200

    test

--- MobilGame/MobilGame.iOS/Main.cs
+++ MobilGame/MobilGame.iOS/Main.cs
@@ -15,6 +15,8 @@ namespace MobilGame.iOS
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
+
+            //gh
         }
     }
 }

946cc7b1d084b655da223db77210f20d925bb629
FabriceMaloyer <fabrice.maloyer@viacesi.fr>
2018-05-24 09:18:51 +0200

    test

--- MobilGame/MobilGame.Android/MainActivity.cs
+++ MobilGame/MobilGame.Android/MainActivity.cs
@@ -20,7 +20,7 @@ namespace MobilGame.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
+            LoadApplication(new App()); // test
         }
     }
 }
